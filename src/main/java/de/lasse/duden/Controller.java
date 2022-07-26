c6ff9bb4ec691900c3476aba2032b4515254e0ce
LasseTimmermanns <lasse.mainkonto@gmail.com>
2022-07-26 09:58:38 +0200

    added utilization query

--- src/main/java/de/lasse/duden/Controller.java
+++ src/main/java/de/lasse/duden/Controller.java
@@ -1,12 +1,9 @@
 package de.lasse.duden;
 
-import de.lasse.duden.database.FilterObj;
 import de.lasse.duden.database.Word;
 import de.lasse.duden.database.WordRepository;
-import org.apache.coyote.Response;
-import org.hibernate.Filter;
-import org.hibernate.Session;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -15,14 +12,14 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class Controller {
 
     @Autowired
@@ -34,7 +31,32 @@ public class Controller {
     @GetMapping("/foo")
     public ResponseEntity<String> getFoooooo(@RequestParam("word") String word_key) {
         HttpHeaders responseHeaders = new HttpHeaders();
-        return new ResponseEntity<String>("BAR", responseHeaders, HttpStatus.OK);
+        //JSONArray jsonArray = new JSONArray(wordRepository.findAllDistinctUtilization());
+        return new ResponseEntity<String>(wordRepository.findAllDistinctUtilization().toString(), responseHeaders, HttpStatus.OK);
+    }
+
+    @GetMapping("/utilization")
+    public ResponseEntity<String> getUtilization(@RequestParam("limit") Optional<Integer> limitParam) {
+        HttpHeaders responseHeaders = new HttpHeaders();
+
+        int limitValue = limitParam.orElse(1000);
+
+        List<String> in = wordRepository.findAllDistinctUtilization();
+        JSONArray out = new JSONArray();
+
+        int i = 0;
+        for(String s : in){
+            if(i > limitValue) break;
+            String[] parts = s.split(",");
+            if(parts[0] == "null") continue;
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("utilization", parts[0]);
+            jsonObject.put("count", parts[1]);
+            out.put(jsonObject);
+            i++;
+        }
+
+        return new ResponseEntity<String>(out.toString(), responseHeaders, HttpStatus.OK);
     }
 
     @GetMapping("/getWords")
--- src/main/java/de/lasse/duden/database/WordRepository.java
+++ src/main/java/de/lasse/duden/database/WordRepository.java
@@ -1,14 +1,16 @@
 package de.lasse.duden.database;
 
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.HashMap;
 import java.util.List;
 
 @Repository
 public interface WordRepository extends CrudRepository<Word, String> {
-
-    public Word findByWord(String word);
     public List<Word> findAll();
+    @Query("select utilization, count(*) from Word group by utilization order by COUNT(*) desc")
+    public List<String> findAllDistinctUtilization();
 
 }
