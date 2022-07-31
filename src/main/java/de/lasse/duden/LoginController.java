0e8b673e77a9a533a3fa5747d7d11319d2c3eb66
LasseTimmermanns <lasse.mainkonto@gmail.com>
2022-07-31 19:15:35 +0200

    added displayMessage to login / register response

--- src/main/java/de/lasse/duden/LoginController.java
+++ src/main/java/de/lasse/duden/LoginController.java
@@ -27,33 +27,33 @@ public class LoginController {
                                              @RequestParam("password") String password) {
 
         if(userExists(username))
-            return createResponse("user already exists", 401);
+            return createResponse("user already exists", "Nutzer existiert bereits", 401);
 
         if(!username.matches("[a-zA-Z0-9_-]*"))
-            return createResponse("username contains unallowed characters", 400);
+            createResponse("username has invalid characters", "Nutzername beinhaltet verbotene Zeichen", 400);
 
         if(username.length() < 3)
-            return createResponse("username has to be bigger than 3", 400);
+            return createResponse("username has to be bigger than 3", "Nutzername muss mindestens drei Zeichen haben", 400);
 
         if(password.length() < 8)
-            return createResponse("password has to be bigger than 8", 400);
+            return createResponse("password has to be bigger than 8", "Passwort muss mindestens acht Zeichen haben", 400);
 
-        if(!password.matches("[a-zA-Z0-9_-]*"))
-            return createResponse("username has invalid characters", 400);
+
 
 
         User user = new User(username, password);
         userRepository.save(user);
 
-        return createResponse("created User", 200);
+        return createResponse("created User", "Nutzer erfolgreich erstellt", 200);
     }
 
-    ResponseEntity<String> createResponse(String message, int responseCode){
-        return createResponse(message, new JSONObject(), responseCode);
+    ResponseEntity<String> createResponse(String message, String displayMessage, int responseCode){
+        return createResponse(message, displayMessage, new JSONObject(), responseCode);
     }
-    ResponseEntity<String> createResponse(String message, JSONObject jsonObject, int responseCode){
+    ResponseEntity<String> createResponse(String message, String displayMessage, JSONObject jsonObject, int responseCode){
         jsonObject.put("response_code", responseCode);
         jsonObject.put("message", message);
+        jsonObject.put("display_message", displayMessage);
 
         return new ResponseEntity<String>(jsonObject.toString(), new HttpHeaders(), responseCode);
     }
@@ -64,17 +64,17 @@ public class LoginController {
                                         @RequestParam("password") String password){
 
         if(!userExists(username))
-            return createResponse("no user found with this username", 400);
+            return createResponse("no user found with this username", "Kein Nutzer mit diesem Nutzernamen gefunden", 400);
 
         User user = userRepository.findUserByUsernameAndPassword(username, password.hashCode());
 
         if(user == null)
-            return createResponse("password doesnt match to username", 400);
+            return createResponse("password doesnt match to username", "Passwort passt nicht zum Nutzernamen", 400);
 
         JSONObject response = new JSONObject();
         response.put("username", user.getUsername());
         response.put("uuid", user.getUuid());
-        return createResponse("User found", response, 200);
+        return createResponse("User found", "Nutzer wird eingeloggt", response, 200);
     }
 
 
